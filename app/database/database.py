1331b8bdc56369cfc2eddf925d6fa83b89bd2771
AntonioBinanti <BNNNTN00M30C351G@studium.unict.it>
2023-11-28 10:54:26 +0100

    Cambio database (da locale a esterno)

--- app/database/database.py
+++ app/database/database.py
@@ -10,8 +10,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 #%%
-#SQLALCHEMY_DATABASE_URL = "postgresql://utente1:gBudzRyqR1ebZVOY2TobUyJINDB3Sqac@dpg-cleu9jbl00ks739tvo20-a/prova_urov" #PER RENDER.COM
-SQLALCHEMY_DATABASE_URL = "sqlite:///./books.db" #PER DB LOCALE
+SQLALCHEMY_DATABASE_URL = "postgresql://utente1:gBudzRyqR1ebZVOY2TobUyJINDB3Sqac@dpg-cleu9jbl00ks739tvo20-a/prova_urov" #PER RENDER.COM
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./books.db" #PER DB LOCALE
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL#, connect_args={"check_same_thread": False}
