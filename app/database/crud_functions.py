5aaff5e504b229272847429b24e4ff90341ddbf4
AntonioBinanti <BNNNTN00M30C351G@studium.unict.it>
2023-11-28 12:59:18 +0100

    Inserimento ulteriori API CRUD

--- app/database/crud_functions.py
+++ app/database/crud_functions.py
@@ -13,6 +13,9 @@ from . import schemas, models
 def get_user(db: Session, username: str):
     return db.query(models.AllUsers).filter(models.AllUsers.username == username).first()
 
+def get_user_id(db: Session, user_id: int):
+    return db.query(models.AllUsers).filter(models.AllUsers.user_id == user_id).first()
+
 def get_users(db: Session):
     return db.query(models.AllUsers).all()
 
@@ -31,13 +34,21 @@ def create_user(db: Session, user: schemas.User):
     db.refresh(user_model)
     return user_model
 
-def get_device(db: Session, identifier: str):
+#def get_devices_user(db: Session, user_id: int):
+#    return db.query(model.AllUsers).filter(models.AllUsers == user_id).
+
+def get_device_model(db: Session, model: str):
+    return db.query(models.Device_info).filter(models.Device_info.model == model).all()
+
+def get_device_model_user(db: Session, model: str, user_id: int):
+    return db.query(models.Device_info).filter(models.Device_info.model == model, models.Device_info.owner_id == user_id).first()
+
+def get_device_identifier(db: Session, identifier: int):
     return db.query(models.Device_info).filter(models.Device_info.identifier == identifier).first()
 
 def get_devices(db: Session):
     return db.query(models.Device_info).all()
 
-#SISTEMARE DA QUI
 def create_device_to_user(db: Session, device: schemas.Device, user_id: int):
     device_model = models.Device_info(**device.dict(), owner_id=user_id)
     db.add(device_model)
@@ -45,3 +56,20 @@ def create_device_to_user(db: Session, device: schemas.Device, user_id: int):
     db.refresh(device_model)
     return device_model
 
+def update_user(db: Session, user_updated: schemas.User, db_user: schemas.UserExt):
+    user_data = user_updated.dict(exclude_unset = True) #Filtraggio dei soli valori inseriti dall'utente
+    for key, value in user_data.items():
+        setattr(db_user, key, value)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def update_device(db: Session, device_updated: schemas.Device, db_device: schemas.DeviceExt):
+    device_data = device_updated.dict(exclude_unset = True) #Filtraggio dei soli valori inseriti dall'utente
+    for key, value in device_data.items():
+        setattr(db_device, key, value)
+    db.add(db_device)
+    db.commit()
+    db.refresh(db_device)
+    return db_device
--- app/main.py
+++ app/main.py
@@ -45,16 +45,23 @@ def predict_comp(payload: schemas.UserPref):
     components_dict = predict_components(payload.user_id, payload.device_info_id)
     return {"Scorciatoie": components_dict}
 
-#%% API per il database
+#%% API CRUD per il database
 
 @app.get("/users", response_model = list[schemas.UserExt])
 def get_users(db: Session = Depends(get_db)):
     db_users = crud_functions.get_users(db)
     return db_users
 
-@app.get("/user/{username}", response_model=schemas.UserExt)
+@app.get("/user_username/{username}", response_model=schemas.UserExt)
 def get_user(username: str, db: Session = Depends(get_db)):
     db_user = crud_functions.get_user(db, username=username)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@app.get("/user_id/{user_id}", response_model=schemas.UserExt)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud_functions.get_user_id(db, user_id = user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
@@ -63,7 +70,21 @@ def get_user(username: str, db: Session = Depends(get_db)):
 def get_devices(db: Session = Depends(get_db)):
     db_devices = crud_functions.get_devices(db)
     return db_devices
+
+@app.get("/device_model/{model}", response_model = list[schemas.DeviceExt])
+def get_device_model(model: str, db: Session = Depends(get_db)):
+    db_devices = crud_functions.get_device_model(db, model = model)
+    if db_devices is None:
+        raise HTTPException(status_code=404, detail="Model not found")
+    return db_devices
     
+@app.get("/device_id/{identifier}", response_model = schemas.DeviceExt)
+def get_device_model(identifier: int, db: Session = Depends(get_db)):
+    db_devices = crud_functions.get_device_identifier(db, identifier = identifier)
+    if db_devices is None:
+        raise HTTPException(status_code=404, detail="Identifier not found")
+    return db_devices
+
 @app.post("/add_user", response_model = schemas.UserExt)
 def create_user(user: schemas.User, db: Session = Depends(get_db)):
     db_user = crud_functions.get_user(db, username = user.username)
@@ -82,4 +103,44 @@ def create_device_to_user(username: str, device: schemas.Device, db: Session = D
             raise HTTPException(status_code=400, detail="Device already registered")
     return crud_functions.create_device_to_user(db=db, device=device, user_id=db_user.user_id)
 
+@app.put("/update_user/{username}") 
+def update_user(username: str, user_updated: schemas.User, db: Session = Depends(get_db)):
+    db_user = crud_functions.get_user(db, username = username)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return crud_functions.update_user(db=db, user_updated=user_updated, db_user=db_user)
+
+@app.put("/update_device_user/{model}/{username}") 
+def update_device(model: str, username: str, device_updated: schemas.Device, db: Session = Depends(get_db)):
+    db_user = crud_functions.get_user(db, username = username)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_device = crud_functions.get_device_model_user(db, model = model, user_id = db_user.user_id)
+    if db_device is None:
+        raise HTTPException(status_code=404, detail="Device not found")
+    return crud_functions.update_device(db=db, device_updated=device_updated, db_device = db_device)
+
+@app.delete("/user/{username}")
+def delete_user(username: str, db: Session = Depends(get_db)):
+    db_user = crud_functions.get_user(db, username = username)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    #Fare prima delete di tutti i devices associati
+    for dev in db_user.device_info:
+        db.delete(dev)
+    db.delete(db_user)
+    db.commit()
+    return {"ok": True}
+
+@app.delete("/device/{model}/{username}")
+def delete_device(model: str, username: str, db: Session = Depends(get_db)):
+    db_user = crud_functions.get_user(db, username = username)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_device = crud_functions.get_device_model_user(db, model = model, user_id = db_user.user_id)
+    if db_device is None:
+        raise HTTPException(status_code=404, detail="Device not found")
+    db.delete(db_device)
+    db.commit()
+    return {"ok": True}
 #%% Test API
